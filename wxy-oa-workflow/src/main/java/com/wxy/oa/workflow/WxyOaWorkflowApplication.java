ab788bbd4e90e4b69918922d721c0ae97dc856cc
dongqingsong <dongbrad@163.com>
2022-05-08 18:10:04 +0800

    init

---
+++ wxy-oa-workflow/src/main/java/com/wxy/oa/workflow/WxyOaWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.wxy.oa.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxyOaWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxyOaWorkflowApplication.class, args);
+    }
+
+}
